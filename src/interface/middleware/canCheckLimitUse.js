b52469a2c41cf2b8b240a3c173bfdad652b58877
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-03-15 16:54:30 +0530

    Added authentication for storage claim

--- src/interface/middleware/canCheckLimitUse.js
+++ src/interface/middleware/canCheckLimitUse.js
@@ -1,5 +1,19 @@
 async function canCheckLimitUse(req, res, next) {
-  next();
+  const invokerAddress = req.invokerAddress;
+  const contractAddress = req.contractAddress;
+  if (req.isAuthenticated) {
+    next();
+  } else {
+    let statusCode = 403;
+    if (!invokerAddress) {
+      statusCode = 401;
+    }
+    return ErrorHandler.throwError({
+      code: statusCode,
+      message: `${invokerAddress} does not have permission to check limit use for portal ${contractAddress}`,
+      req,
+    });
+  }
 }
 
 module.exports = canCheckLimitUse;
--- src/interface/middleware/canUpdateLimit.js
+++ src/interface/middleware/canUpdateLimit.js
@@ -1,5 +1,19 @@
 async function canUpdateLimit(req, res, next) {
-  next();
+  const invokerAddress = req.invokerAddress;
+  const contractAddress = req.contractAddress;
+  if (req.isAuthenticated) {
+    next();
+  } else {
+    let statusCode = 403;
+    if (!invokerAddress) {
+      statusCode = 401;
+    }
+    return ErrorHandler.throwError({
+      code: statusCode,
+      message: `${invokerAddress} does not have permission to update limit for portal ${contractAddress}`,
+      req,
+    });
+  }
 }
 
 module.exports = canUpdateLimit;
