d30a0d751bd204c71e902ee4b5c71f14fcd5092c
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-10-12 06:38:11 +0530

    Removed Logs

--- src/domain/task/completeTask.js
+++ src/domain/task/completeTask.js
@@ -4,7 +4,6 @@ async function completeTask({ contractAddress, invokerAddress, taskId }) {
   const taskStatus = await Task.findOne({ contractAddress }).lean();
   const taskMap = taskStatus && taskStatus.taskMap || {};
   taskMap[taskId] = invokerAddress;
-  console.log(taskMap);
   await Task.findOneAndUpdate(
     { contractAddress },
     { $set: { taskMap } },
