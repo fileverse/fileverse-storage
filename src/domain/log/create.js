1c90a7154a4aef13e3ccbf14ab876d89ddc79eb7
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-10-12 06:44:56 +0530

    hooked log precessing to log creation

--- src/domain/log/create.js
+++ src/domain/log/create.js
@@ -1,9 +1,12 @@
 const { Log } = require('../../infra/database/models');
+const process = require('./process');
 
 async function create(eventName, { fileId, contractAddress, ipfsHash, tags }) {
   const logObject = await new Log({ eventName, fileId, contractAddress, ipfsHash, tags }).save();
+  const safeLog = logObject.safeObject();
   // process logs
-  return logObject.safeObject();
+  await process(contractAddress, 'system', safeLog)
+  return safeLog;
 }
 
 module.exports = create;
