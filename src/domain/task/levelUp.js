37189dd1e2c8218520390deea7b4d77e0c62782b
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-10-15 17:21:07 +0530

    Increase storage limit on level up

--- src/domain/task/levelUp.js
+++ src/domain/task/levelUp.js
@@ -1,13 +1,25 @@
-const { Task } = require("../../infra/database/models");
-const { getRank } = require('./tasks');
+const { Task, Limit } = require("../../infra/database/models");
+const { getRank, getStorage } = require('./tasks');
 const getTaskStatus = require('./getTaskStatus');
 
+async function updateStorageLimit({ contractAddress, addedStorage }) {
+  await Limit.findOneAndUpdate(
+    { contractAddress },
+    { $inc: { storageLimit } },
+  );
+}
+
 async function levelUp({ contractAddress, invokerAddress }) {
-  const { collectedPoints, canLevelUp } = await getTaskStatus({ contractAddress, invokerAddress });
-  const newRank = getRank({ collectedPoints });
+  const { collectedPoints, canLevelUp, rank: currentRank } = await getTaskStatus({ contractAddress, invokerAddress });
   if (!canLevelUp) {
     return false;
   }
+  const currentStorage = getStorage({ rank: currentRank });
+  const newRank = getRank({ collectedPoints });
+  const newStorage = getStorage({ rank: newRank });
+  const addedStorage = newStorage - currentStorage;
+  // add to the limit
+  await updateStorageLimit({ contractAddress, addedStorage });
   await Task.findOneAndUpdate(
     { contractAddress },
     { $set: { rank: newRank } },
