b259b758f5e8eade52540835219040c7b9e8ae8b
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-04-11 15:24:33 +0530

    using @aws-sdk/client-s3 package to handle, this is working

--- src/domain/ipfs/filebase.js
+++ src/domain/ipfs/filebase.js
@@ -1,4 +1,4 @@
-const AWS = require('aws-sdk');
+const { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const config = require('../../../config');
 const IpfsStorageInterface = require('./interface');
 
@@ -7,22 +7,18 @@ class FileBase extends IpfsStorageInterface {
         super();
         this.accessKey = config.FILEBASE_ACCESS_KEY
         this.secret = config.FILEBASE_SECRET
-        this.access_token = this.get_access_token()
 
-        this.s3 = new AWS.S3({
-            apiVersion: '2006-03-01',
-            accessKeyId: this.accessKey,
-            secretAccessKey: this.secret,
+        this.s3 = new S3Client({
             endpoint: 'https://s3.filebase.com',
             region: 'us-east-1',
             signatureVersion: 'v4',
-        })
+            credentials: {
+                accessKeyId: this.accessKey,
+                secretAccessKey: this.secret,
+            },
+        });
     }
 
-    get_access_token() {
-        let passkey = this.accessKey + ":" + this.secret;
-        return btoa(passkey);
-    }
 
     async upload(readableStreamForFile, { name, attribute }) {
         let bucket_name = attribute ? attribute : "test-fileverse"
@@ -31,26 +27,33 @@ class FileBase extends IpfsStorageInterface {
             Bucket: bucket_name,
             Key: name,
             Body: readableStreamForFile,
-            Metadata: { import: "car" }
         };
 
-        const request = this.s3.putObject(uploadParams, function (err, data) {
-            if (err) {
-                console.log("Error", err.message);
-            }
-            else if (data) {
-                console.log("Upload Success", data.Location);
-            }
-            else {
-                console.log("Something else")
-            }
-        });
+        const command = new PutObjectCommand(uploadParams);
+        let cid = null;
+        let ts = null;
+        command.middlewareStack.add(
+            (next) => async (args) => {
+                const response = await next(args);
+                if (!response.response.statusCode) return response;
+                // Get cid from headers
+                cid = response.response.headers["x-amz-meta-cid"];
+                ts = response.response.headers["date"];
 
-        request.on('httpHeaders', (statusCode, headers) => {
-            console.log(`CID: ${headers['x-amz-meta-cid']}`);
-        });
-        request.send();
+                return response;
+            },
 
+        );
+
+        await Promise.resolve(this.s3.send(command));
+
+        return {
+            ipfsUrl: `https://w3s.link/ipfs/${cid}/${name}`,
+            ipfsHash: `${cid}/${name}`,
+            ipfsStorage: 'filebase',
+            pinSize: null,
+            timestamp: (new Date(ts)).getTime(),
+        };
     }
 
 
@@ -60,24 +63,32 @@ class FileBase extends IpfsStorageInterface {
             Bucket: "test-fileverse"
         };
 
-        try {
-            this.s3.getObject(params, function (error, data) {
-                if (error) {
-                    console.log("Error while reading file " + key + ":" + bucket);
-                    return callback("Error!");
-                } else {
-                    console.log("Returning contents from " + key + ":" + bucket);
-                    return callback(Buffer.from(data.Body, 'utf8').toString());
-                }
-            });
-        } catch (error) {
-            console.error(error);
-            return callback("Error!");
-        }
+        const command = new GetObjectCommand(params);
+        command.middlewareStack.add(
+            (next) => async (args) => {
+                const response = await next(args);
+                if (!response.response.statusCode) return response;
+                // Get cid from headers
+                console.log(response);
+
+                return response;
+            },
+
+        );
+
+        await Promise.resolve(this.s3.send(command));
+
     }
 
     async remove({ ipfsHash }) {
-        return;
+        let hashes = ipfsHash.split('/');
+        let params = {
+            Key: hashes[0],
+            Bucket: "test-fileverse"
+        };
+
+        const command = new DeleteObjectCommand(params);
+        await Promise.resolve(this.s3.send(command));
     }
 }
 
