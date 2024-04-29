d9606240224cf295a8b8ed85be077581a3e0980b
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-04-29 21:48:24 +0530

    revert: src/infra/ucan.js to last working checkpoint

--- src/infra/ucan.js
+++ src/infra/ucan.js
@@ -8,88 +8,53 @@ const serviceDID = config.SERVICE_DID;
 let verify = (req, res, next) => {
   req.requestId = uuidv4();
   console.log('req.requestId: ', req.requestId);
-
   let token = req.headers['authorization']; // Express headers are auto converted to lowercase
   if (token && token.startsWith('Bearer ')) {
     // Remove Bearer from string
     token = token.slice(7, token.length);
   }
-  const contractAddress = req.headers?.contract;
-  const invokerAddress = req.headers?.invoker;
-  const chainId = req.headers?.chain;
+  const contractAddress = req.headers && req.headers.contract;
+  const invokerAddress = req.headers && req.headers.invoker;
+  const chainId = req.headers && req.headers.chain;
   req.isAuthenticated = false;
   req.invokerAddress = invokerAddress;
   req.contractAddress = contractAddress;
   req.chainId = chainId;
-
-  // request remains not authenticated if no token is provided
-  if (!token) {
-    next()
-    return
-  }
-
-  if (contractAddress) {
-    collaboratorKey({
-      contractAddress, invokerAddress, chainId
-    }).then((invokerDID) => {
-      if (invokerDID) {
-        ucans.verify(token, {
-          // to make sure we're the intended recipient of this UCAN
-          audience: serviceDID,
-          // capabilities required for this invocation & which owner we expect for each capability
-          requiredCapabilities: [
-            {
-              capability: {
-                with: { scheme: "storage", hierPart: contractAddress.toLowerCase() },
-                can: { namespace: "file", segments: ["CREATE"] }
-              },
-              rootIssuer: invokerDID,
+  if (token && contractAddress) {
+    collaboratorKey({ contractAddress, invokerAddress, chainId })
+      .then((invokerDID) => {
+        if (invokerDID) {
+          ucans.verify(token, {
+            // to make sure we're the intended recipient of this UCAN
+            audience: serviceDID,
+            // capabilities required for this invocation & which owner we expect for each capability
+            requiredCapabilities: [
+              {
+                capability: {
+                  with: { scheme: "storage", hierPart: contractAddress.toLowerCase() },
+                  can: { namespace: "file", segments: ["CREATE"] }
+                },
+                rootIssuer: invokerDID,
+              }
+            ],
+          }).then((result) => {
+            console.log(result);
+            if (result.ok) {
+              req.isAuthenticated = true;
             }
-          ],
-        }).then((result) => {
-          console.log(result);
-          if (result.ok) {
-            req.isAuthenticated = true;
-          }
+            next();
+          });
+        } else {
           next();
-        });
-      } else {
-        next();
-      }
-    }).catch((error) => {
-      console.log(error);
-      next();
-    });
-  } else {
-    /* handle temp user auth
-      * here the invoker address is expected to be public DID of the temp user
-      * and the ucan token should be issued by the service DID
-    */
-    ucans.verify(token, {
-      audience: serviceDID,
-      requiredCapabilities: [
-        {
-          capability: {
-            with: { scheme: "storage", hierPart: invokerAddress.toLowerCase() },
-            can: { namespace: "file", segments: ["CREATE"] }
-          },
-          rootIssuer: invokerAddress.toLowerCase(),
         }
-      ],
-    }).then((result) => {
-      console.log(result);
-      if (result.ok) {
-        req.isAuthenticated = true;
-      }
-    }).catch((error) => {
-      console.log(error);
-    });
+      })
+      .catch((error) => {
+        console.log(error);
+        next();
+      });
+  } else {
+    next();
   }
-
-
-  next();
 };
-
-
 
 module.exports = { verify };
