c0afc372a1bd21169cfc1add78232edce17b7bbc
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-06-11 11:53:33 +0530

    updated slack message for the errors

--- src/infra/reporter.js
+++ src/infra/reporter.js
@@ -1,9 +1,15 @@
-const config = require('../../config');
 const axios = require('axios');
+const config = require('../../config');
+
+const slackHook = config.SLACK_REPORTER;
+const env = config.HOST ? config.HOST : 'local';
 
 async function reportError(message) {
-  env = config.HOST ? config.HOST : 'local';
-  slackHook = config.SLACK_REPORTER;
+  if (slackHook === undefined) {
+    console.log('Error: Slack hook not found');
+    return;
+  }
+
   if (env !== 'local') {
     await axios.post(slackHook, {
       "blocks": [
@@ -11,7 +17,7 @@ async function reportError(message) {
           "type": "section",
           "text": {
             "type": "mrkdwn",
-            "text": "*Error* ```" + message + "```\n*Environment:* " + env.toUpperCase()
+            "text": "*Error* " + env.toUpperCase() + "\n```" + message + "```"
           }
         }
       ]
