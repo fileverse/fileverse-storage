65ddc09e60cf1c3b34eb09f36c76a74e3b8b52ce
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-07-05 08:39:00 +0530

    ddoc create for analytics

--- src/interface/log/index.js
+++ src/interface/log/index.js
@@ -8,7 +8,9 @@ const router = express.Router();
 const canView = require('../middleware/canView');
 
 const create = require('./create');
+const ddocCreate = require('./ddoc')
 
 router.post('/create', asyncHandler(canView), asyncHandlerArray(create));
+router.get('/create/ddoc', asyncHandlerArray(ddocCreate));
 
 module.exports = router;
