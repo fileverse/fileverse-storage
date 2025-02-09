f4abbb6ab7de62bb1e56ea368f5b54deff5bb4a5
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2025-02-09 17:09:51 +0530

    TEC-47: Onchain Comments for DDocs (#182)
    Upload endpoint for Comments
    Public endpoint to allow anyone to comment on the docs 
    ---------
    Co-authored-by: Joshua Onwuzu <joshuaonwuzu75@gmail.com>

---
+++ src/interface/middleware/canComment.js
@@ -0,0 +1,5 @@
+async function canComment(req, res, next) {
+  next();
+}
+
+module.exports = canComment;
---
+++ src/interface/upload/comment.js
@@ -0,0 +1,44 @@
+const { upload } = require("../../domain");
+const { validator } = require("../middleware");
+const { Joi, validate } = validator;
+const ErrorHandler = require("../../infra/errorHandler");
+
+const uploadValidation = {
+  headers: Joi.object({
+    invoker: Joi.string().optional(),
+  }).unknown(true),
+};
+const commentSchema = Joi.object({
+  encryptedData: Joi.string().allow("").required(),
+  id: Joi.string().required(),
+  username: Joi.string().required(),
+});
+
+async function uploadCommentFn(req, res) {
+  const file = req.files?.file;
+  if (file.mimetype !== "application/json") {
+    return ErrorHandler.throwError({
+      code: 400,
+      message: `File must be a JSON file`,
+      req,
+    });
+  }
+
+  const jsonData = JSON.parse(file.data.toString());
+  const { error } = commentSchema.validate(jsonData);
+
+  if (error) {
+    return ErrorHandler.throwError({
+      code: 400,
+      message: error.details[0].message,
+      req,
+    });
+  }
+
+  const createdFile = await upload({
+    file: req.files?.file,
+  }).catch(console.log);
+  res.json(createdFile);
+}
+
+module.exports = [validate(uploadValidation), uploadCommentFn];
